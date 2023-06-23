9709b4b6b21f9aafd5284852d4806d944e2e6a97
YushiKoyama <ys.koyama@core-tech.jp>
2023-06-23 13:39:14 +0900

    admin複数

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,7 +14,7 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('admin')->insert([
+        DB::table('admins')->insert([
             [
                 'name' => 'admin',
                 'number' => 00011223344,
