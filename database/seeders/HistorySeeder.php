2a5e44f0ee920d7acb807cde4fec9f0493835602
nomuramotomasa <m.nomura@core-tech.jp>
2023-06-23 13:03:26 +0900

    historyとcart　ダミー

--- database/seeders/HistorySeeder.php
+++ database/seeders/HistorySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class HistorySeeder extends Seeder
 {
@@ -12,6 +14,28 @@ class HistorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('historys')->insert([
+            [
+                'user_id' => 1,
+                'product_id' => 1,
+                'amount' => 3,
+                'purchased_at' => '2000-06-11',
+                'total_price' => 1000,
+            ],
+            [
+                'user_id' => 2,
+                'product_id' => 2,
+                'amount' => 3,
+                'purchased_at' => '2000-06-14',
+                'total_price' => 2000,
+            ],
+            [
+                'user_id' => 3,
+                'product_id' => 3,
+                'amount' => 3,
+                'purchased_at' => '1999-09-27',
+                'total_price' => 3000,
+            ],
+        ]);
     }
 }
