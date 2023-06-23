412b8071df9937fc677fe0de9313c2ec2f8f968d
nomuramotomasa <m.nomura@core-tech.jp>
2023-06-23 18:30:19 +0900

    linkまとめ

---
+++ resources/views/components/pay-link.blade.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a class="text-blue-400" href="{{ route('product.index')}}">商品一覧画面</a></li>
+    <li><a class="text-blue-400" href="{{ route('product.show')}}"">商品詳細</a></li>
+    <li><a class="text-blue-400" href="{{ route('history.index')}}">購入履歴</a></li>
+    <li><a class="text-blue-400" href="{{ route('pay.checkout')}}">商品購入画面</a></li>
+</ul>
