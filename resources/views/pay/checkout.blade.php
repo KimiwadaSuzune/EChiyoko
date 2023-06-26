a24b483eec2dfcc3d4b92dd0f5d4500cab025de1
YushiKoyama <ys.koyama@core-tech.jp>
2023-06-26 11:50:07 +0900

    購入画面レイアウト変更

--- resources/views/pay/checkout.blade.php
+++ resources/views/pay/checkout.blade.php
@@ -1,12 +1,25 @@
 <x-app-layout>
-@foreach($user->product as $product_data)
-<tr>
-    <br>
-    <td>{{$product_data->id}}</td>
-    <br>
-    <td>{{$product_data->name}}</td>
-    <br>
-    <td>{{$product_data->pivot->amount}}</td>
-</tr>
-@endforeach
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            購入画面
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    @foreach($user->product as $product_data)
+                    <tr>
+                        <br>
+                        <td>{{$product_data->id}}</td>
+                        <br>
+                        <td>{{$product_data->name}}</td>
+                        <br>
+                        <td>{{$product_data->pivot->amount}}</td>
+                    </tr>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
