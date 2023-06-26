b77c3018d6de8095e92a7291535bd25d268ada0c
YushiKoyama <ys.koyama@core-tech.jp>
2023-06-26 16:08:16 +0900

    ユーザーのルート変更

--- routes/web.php
+++ routes/web.php
@@ -22,24 +22,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth:users')->group(function () {
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //商品購入関連
+    Route::get('/pay/checkout', [PayController::class, 'index'])->name('pay.checkout');
+    Route::post('/pay/checkout', [PayController::class, 'store'])->name('pay.store');
+    Route::get('/pay/success', [PayController::class, 'success'])->name('pay.success');
+
+    //kimiwada
+    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
+    Route::get('/history', [HistoryController::class, 'index'])->name('history.index');
 });
 
-//商品購入関連
-Route::get('/pay/checkout', [PayController::class, 'index'])->name('pay.checkout');
-Route::post('/pay/checkout', [PayController::class, 'store'])->name('pay.store');
-Route::get('/pay/success', [PayController::class, 'success'])->name('pay.success');
-
 require __DIR__.'/auth.php';
-
-//kimiwada
-Route::get('/product', [ProductController::class, 'index'])->name('product.index');
-Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
-Route::get('/history', [HistoryController::class, 'index'])->name('history.index');
