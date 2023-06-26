77a387f1ab099e24546cf7e2949fc328f3c17388
KimiwadaSuzune <s.kimiwada@core-tech.jp>
2023-06-26 13:51:19 +0900

    Blade 変更

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -15,15 +15,17 @@
                         <h3 class="text-lg font-semibold mb-4">All Products</h3>
                         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                             @foreach ($products as $product)
-                            <a href="{{ route('product.show', ['id' => $product->id]) }}" class="col-lg-4 col-md-6">
-                                    <div>
-                                        <img src="{{ asset($product->image) }}" alt="{{ $product->name }}" class="mx-auto mb-4">
-                                        <div class="text-center">
-                                            <p class="text-sm">{{ $product->name }}</p>
-                                            <p class="text-sm">¥{{ number_format($product->price) }}</p>
+                                <div class="col-span-1">
+                                    <a href="{{ route('product.show', ['id' => $product->id]) }}">
+                                        <div class="bg-white rounded-lg shadow-md">
+                                            <img src="{{ asset($product->filepass) }}" alt="{{ $product->name }}" class="mx-auto my-4 w-32 h-32 object-cover">
+                                            <div class="p-4">
+                                                <p class="text-sm">{{ $product->name }}</p>
+                                                <p class="text-sm">¥{{ number_format($product->price) }}</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                </a>
+                                    </a>
+                                </div>
                             @endforeach
                         </div>
                     </div>
