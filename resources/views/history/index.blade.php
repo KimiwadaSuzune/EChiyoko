547c01445641ef1e7f81d9fb6a639e1a56f39ff1
KimiwadaSuzune <s.kimiwada@core-tech.jp>
2023-06-26 11:43:43 +0900

    キミワダ　更新

--- resources/views/history/index.blade.php
+++ resources/views/history/index.blade.php
@@ -1,30 +1,44 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>購入履歴</title>
-</head>
-<body>
-    <h1>購入履歴</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            商品一覧
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="mb-8">
+                        <x-pay-link/>
+                    </div>
+                    <head>
+                        <title>購入履歴</title>
+                    </head>
+                    <body>
+                        <h1>購入履歴</h1>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>商品名</th>
+                                    <th>価格</th>
+                                    <th>購入日時</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($histories as $history)
+                                    <tr>
+                                        <td>{{ $history->product->filepass }}</td>
+                                        <td>{{ $history->product->name }}</td>
+                                        <td>{{ $history->product->price }}</td>
+                                        <td>{{ $history->created_at }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </body>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
 
-    <table>
-        <thead>
-            <tr>
-                <th>注文番号</th>
-                <th>商品名</th>
-                <th>価格</th>
-                <th>購入日時</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($histories as $history)
-                <tr>
-                    <td>{{ $history->product->filepass }}</td>
-                    <td>{{ $history->product->name }}</td>
-                    <td>{{ $history->product->price }}</td>
-                    <td>{{ $history->created_at }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</body>
-</html>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            商品一覧画面
+            商品一覧
         </h2>
     </x-slot>
     <div class="py-12">
@@ -9,13 +9,13 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div class="mb-8">
-                        <x-pay-link/>
+
                     </div>
                     <div class="text-center">
                         <h3 class="text-lg font-semibold mb-4">All Products</h3>
                         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                             @foreach ($products as $product)
-                                <a href="#" class="border border-gray-300 p-4 rounded-md block">
+                            <a href="{{ route('product.show', ['id' => $product->id]) }}" class="col-lg-4 col-md-6">
                                     <div>
                                         <img src="{{ asset($product->image) }}" alt="{{ $product->name }}" class="mx-auto mb-4">
                                         <div class="text-center">
