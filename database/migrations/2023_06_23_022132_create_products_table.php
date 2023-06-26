e380b63b0e0d9c18d57220e4a1aa6589206d6a2a
YushiKoyama <ys.koyama@core-tech.jp>
2023-06-26 13:02:59 +0900

    productのmigrationファイル変更

--- database/migrations/2023_06_23_022132_create_products_table.php
+++ database/migrations/2023_06_23_022132_create_products_table.php
@@ -21,10 +21,6 @@ return new class extends Migration
             $table->foreignId('category_id');
             $table->timestamps();
         });
-
-        Schema::table('products', function (Blueprint $table) {
-            $table->boolean('enabled')->default(true);
-        });
     }
 
     /**
