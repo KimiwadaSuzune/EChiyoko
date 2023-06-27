adff979c07b10791b72667d203aa71fcb1f5ca11
KimiwadaSuzune <s.kimiwada@core-tech.jp>
2023-06-27 11:33:31 +0900

    登録者一覧画面

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Admin\Admin;
-use App\Http\Requests\StoreProductRequest;
-use App\Http\Controllers\Admin\Request;
+use Illuminate\Http\Request;
 use App\Http\Requests\UpdateProductRequest;
 use App\Models\Product;
 
@@ -17,11 +15,9 @@ class AdminProductController extends Controller
     public function index()
     {
 
-        $products = Product::select('id', 'name', 'price')
-        ->where('enabled', true)
-        ->get();
+        $products = Product::all();
 
-        return view('product.index', compact('products'));
+        return view('admin.admin.index', compact('products'));
     }
 
     /**
@@ -29,7 +25,7 @@ class AdminProductController extends Controller
      */
     public function create()
     {
-        return view('product.create');
+        return view('admin.admin.create');
     }
 
     public function store(Request $request)
@@ -43,49 +39,49 @@ class AdminProductController extends Controller
             'category_id' => ['required']
         ]);
 
-        Product::create([
-            'name' => $request->name,
-            'price' => $request->price,
-            'stock' => $request->stock,
-            'filepass' => $request->filepass,
-            'enabled' => $request->enabled,
-            'category_id' => $request->category_id,
-        ]);
+        Product::create($validated);
 
-        return redirect()->route('product.index')
+        return redirect()->route('admin.admin.index')
             ->with('flash_message', '保存しました');
     }
-
     /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
         $product = Product::find($id);
-        return view('product.show', compact('product'));
+        return view('admin.admin.show', compact('product'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product)
+    public function edit(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        return view('admin.admin.edit', compact('product'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProductRequest $request, Product $product)
+    public function update(UpdateProductRequest $request, Product $product,$id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $product->update($request->validated());
+
+        return redirect()->route('admin.admin.index')
+            ->with('flash_message', '登録情報を更新しました！');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy($id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return redirect()->route('admin.admin.index');
     }
 }
