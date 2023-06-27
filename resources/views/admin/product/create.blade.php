55640cc26ae75a707c60c84c7b1dccc62b8c3897
KimiwadaSuzune <s.kimiwada@core-tech.jp>
2023-06-27 16:30:55 +0900

    変更

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -11,9 +11,13 @@
                     <div class="mb-8">
 
                     </div>
-                    <form action="{{ route('admin.product.store') }}" method="POST">
+                    <div>
+                        <form action="{{ route('admin.product.store') }}" method="POST">
                         @csrf
-
+                            <label for="image">画像:</label>
+                            <input type="file" name="image" required>
+                          </form>
+                    </div>
                         <div>
                             <label for="name">商品名:</label>
                             <input type="text" name="name" id="name" required>
@@ -28,12 +32,6 @@
                             <label for="stock">在庫:</label>
                             <input type="number" name="stock" id="stock" required>
                         </div>
-
-                        <div>
-                            <label for="image">画像パス:</label>
-                            <input type="text" name="image" id="image" required>
-                        </div>
-
                         <div>
                             <label for="enabled">表示・非表示:</label>
                             <input type="checkbox" name="enabled" id="enabled">
