a3835931ff72c5ca4164803d68489a51eff516ae
YushiKoyama <ys.koyama@core-tech.jp>
2023-06-28 10:35:51 +0900

    productの一覧view変更

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -5,9 +5,9 @@
         </h2>
     </x-slot>
 
-    <div class="max-w-7xl sm:px-6 lg:px-8 py-8">
-        <div class="bg-white shadow-sm sm:rounded-lg">
-            <div class="p-6 text-center">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-12">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900 text-center">
                 <form method="GET" action="{{ route("product.index")}}">
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label">商品名</label>
